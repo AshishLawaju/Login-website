8728188e0682a5145470a89a0d66bca8a8ef997d
AshishLawaju <ashish.lawaju74@gmail.com>
2023-11-22 12:42:59 +0545

    contact form

--- src/components/slideSection/Customer.jsx
+++ src/components/slideSection/Customer.jsx
@@ -1,36 +1,125 @@
-
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 const Customer = () => {
+  const [contactPage, setContactPage] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneno, setPhoneno] = useState("");
+  const [messages, setMessages] = useState("");
+  const [capVal, setCapVal] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <div className=" h-[100vh] w-full flex items-center ">
-        <div className="text-left text-white   max-md:mt-24 max-md:pl-9 md:mt-24 w-full h-[70%] lg:ml-36 ">
-          <h3 className="text-xl">Customer service</h3>
-          <h2 className="text-4xl mt-4 ">CUSTOMER</h2>
-          <p className="mr-12 text-stone-300 mt-4 max-sm:mt-2">
-            Feel free to reach out to us with any questions. <br/> We are very
-            friendly and always open to <br/> discussing new projects, interesting
-            ideas.
-          </p>
+      {!contactPage && (
+        <div className=" h-[100vh] w-full flex items-center ">
+          <div className="text-left text-white   max-md:mt-24 max-md:pl-9 md:mt-24 w-full h-[70%] lg:ml-36 ">
+            <h3 className="text-xl">Customer service</h3>
+            <h2 className="text-4xl mt-4 ">CUSTOMER</h2>
+            <p className="mr-12 text-stone-300 mt-4 max-sm:mt-2">
+              Feel free to reach out to us with any questions. <br /> We are
+              very friendly and always open to <br /> discussing new projects,
+              interesting ideas.
+            </p>
 
-          <div className="flex gap-40 mt-4 max-sm:flex-col max-sm:gap-3">
-            <div>
-              <h2 className="text-bold text-2xl mb-4 max-sm:mb-0">Contact</h2>
-              <p className="text-slate-300">Call: 9749761111</p>
-          <p className="text-slate-300 sm:hidden">Email: logincorpnepal@gmail.com</p>
+            <div className="flex gap-40 mt-4 max-sm:flex-col max-sm:gap-3">
+              <div>
+                <h2 className="text-bold text-2xl mb-4 max-sm:mb-0">Contact</h2>
+                <p className="text-slate-300">Call: 9749761111</p>
+                <p className="text-slate-300 sm:hidden">
+                  Email: logincorpnepal@gmail.com
+                </p>
+              </div>
+              <div>
+                <h2 className="text-bold text-2xl mb-4 max-sm:mb-0">Address</h2>
+                <p className="text-slate-300">
+                  <span></span>
+                  Samakhusi, Kathmandu
+                </p>
+              </div>
             </div>
-            <div>
-              <h2 className="text-bold text-2xl mb-4 max-sm:mb-0">Address</h2>
-              <p className="text-slate-300">
-                <span></span>
-                Samakhusi, Kathmandu
-              </p>
+            <p className="text-slate-300 mt-4 max-sm:hidden">
+              Email: logincorpnepal@gmail.com
+            </p>
+
+            <button
+              onClick={() => setContactPage(!contactPage)}
+              className="border-2 mb-2  rounded-full p-1.5 pl-4 pr-4 mt-4"
+            >
+              Send messages
+            </button>
+          </div>
+        </div>
+      )}
+      {contactPage && (
+        <div className="w-full h-[100vh]  text-white flex items-center justify-around text-base">
+          <div className="h-[55vh] ">
+            <div className="flex flex-col   gap-16 ">
+              <div className="text-start flex flex-col gap-4">
+                <div className="text-2xl ">Customer Service </div>
+                <div className="uppercase text-4xl ">Email us</div>
+              </div>
+
+              <div>
+                <button
+                  onClick={() => setContactPage(false)}
+                  className="border-2 px-5 py-2 rounded-3xl flex items-start"
+                >
+                  Information
+                </button>
+              </div>
             </div>
           </div>
-          <p className="text-slate-300 mt-4 max-sm:hidden">Email: logincorpnepal@gmail.com</p>
+          <div>
+            <form className="flex flex-col text-start gap-2">
+              <label>Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="bg-transparent pb-1 pt-2 px-3 border border-x-0 border-t-0 focus:outline-none"
+              ></input>
+              <label>Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="bg-transparent pb-1 pt-2 px-3 border border-x-0 border-t-0 focus:outline-none"
+              />
 
-          <button className="border-2 mb-2  rounded-full p-1.5 pl-4 pr-4 mt-4">Send messages </button>
+              <label htmlFor="">Phone No.</label>
+              <input
+                type="number"
+                value={phoneno}
+                onChange={(e) => setPhoneno(e.target.value)}
+                className="bg-transparent pb-1 pt-2 px-3 border border-x-0 border-t-0 focus:outline-none"
+              />
+              <label>Message</label>
+              <textarea
+                type="text"
+                value={messages}
+                onChange={(e) => setMessages(e.target.value)}
+                className="bg-transparent border h-[12vh] rounded-md"
+              ></textarea>
+              <ReCAPTCHA
+                sitekey="6LcoNRgpAAAAAHUQzMIRUxlRbuhhDc6V990-haRI"
+                onChange={(val) => setCapVal(val)}
+                className="bg-transparent"
+              />
+              <button
+                disabled={!capVal}
+                onClick={handleSubmit}
+                className="px-5 py-2 rounded-full bg-[#FF1A75]  hover:bg-red-500"
+              >
+                Send
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
